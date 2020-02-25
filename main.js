245fb0ed6df1cfc00ca4533c90d31a3b3720fa51
David An <andavid94@gmail.com>
2020-02-25 15:31:31 -0800

    misc update

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-
+/*
+** This program 
+*/
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
@@ -7,24 +9,45 @@ var handlebars = require("express-handlebars").create({defaultLayout: "main"});
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
 app.set("port", 4095);
+
 app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
+/*
+** this function send a GET request to the server
+*/
 app.get('/', function(req, response) {
+    
     var qParams = [];
-    
-    for (var p in req.query) {
-        //qParams += "The name is: " + p + " and the value is: " + req.query[p] + ", "; 
-    
-        qParams.push({"The name is: ": p, " and the value is: ": req.query[p]});
-    }
-
-    //qParams = qParams.substring(0, qParams.lastIndexOf(','));
-    //qParams += '.';
     var context = {};
+    for (var p in req.query) {
+        qParams.push({'name': p, 'value': req.query[p]});
+    }
     context.dataList = qParams;
 
-    response.render('get', context);
+    response.render('get-data', context);
+});
+
+/*
+** this function sends a POST request to the server
+*/
+app.post('/', function(req, response) {
+    
+    var qParams = [];
+    for (var p in req.body) {
+        qParams.push({'name': p, 'value': req.body[p]});
+    }
+    
+    /*var bodyList = [];
+    for (var p in req.body) {
+        bodyList.push({'name': p, 'value': req.body[p]});
+    }*/
+
+    var context = {};
+    context.dataList = qParams;
+    //context.bodyList = bodyList;
+
+    response.render('post-data', context);
 });
 
 app.use(function(req, response) {
@@ -38,7 +61,7 @@ app.use(function(err, req, response, next) {
     response.type('text/plain');
     response.status(500);
     response.send('500 - Server Error');
-})
+});
 
 app.listen(app.get('port'), function() {
     console.log('Express started on http://localhost:' + app.get('port'));
